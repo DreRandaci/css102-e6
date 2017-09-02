a6cf67fb091f23100d15a476c31984816bceede0
Dre mf Randaci <drerandaci@gmail.com>
2017-09-02 10:33:45 -0500

    added let and const and arrow functions

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-
-var allProducts = [];
-
-var product1 = {
-	name: "Mop Attire",
-	imagePath: "./images/mopAttire.jpg",
-	imageAlt: "Product: Mop Attire",
-	description: "This is a really good description of our product. It really sells it. It’s the best.",
-	price: 3000,
-	soldOut: false
+// No more var - let, const
+// Fat arrow functions
+// Object literal value shorthand notation
+// String template literals
+const allProducts = [];
+const product1 = {
+  name: "Mop Attire",
+  imagePath: "./images/mopAttire.jpg",
+  imageAlt: "Product: Mop Attire",
+  description: "This is a really good description of our product. It really sells it. It's the best.",
+  price: 3000,
+  soldOut: false
 };
-var product2 = {
+const product2 = {
   name: "Taco Suit",
   imagePath: "./images/tacoSuit.jpg",
   imageAlt: "Product: Taco Suit",
@@ -17,7 +19,7 @@ var product2 = {
   price: 25,
   soldOut: false
 };
-var product3 = {
+const product3 = {
   name: "Neck Decoration",
   imagePath: "./images/neckDecoration.jpg",
   imageAlt: "Product: Neck Decoration",
@@ -25,7 +27,7 @@ var product3 = {
   price: 750,
   soldOut: false
 };
-var product4 = {
+const product4 = {
   name: "Head Ornament",
   imagePath: "./images/headOrnament.jpg",
   imageAlt: "Product: Head Ornament",
@@ -33,70 +35,71 @@ var product4 = {
   price: 3.22,
   soldOut: true
 };
-var product5 = {
+const product5 = {
   name: "Boob Hat",
   imagePath: "./images/boobHat.jpg",
   imageAlt: "Product: Boob Hat",
   description: "This is a really good description of our product. It really sells it. It's the best.",
   price: 36,
-  soldOut: true
+  soldOut: false
 };
-// adding all product objects to the array
 allProducts.push(product1);
 allProducts.push(product2);
 allProducts.push(product3);
 allProducts.push(product4);
 allProducts.push(product5);
-
-console.log("All my baby products: ", allProducts);
-
-let productContainer = document.getElementById("product-container");
-
-function buildDomString(product){
-  let domString = "";
-  domString +=  '<section class="product">';
-  domString +=      '<div class="title">';
-  domString +=         '<h2>' + product.name + '</h2>';
-  domString +=      '</div>';
-  domString +=    '<div class="image">';
-  domString +=       '<img src="' + product.imagePath + '" alt="' + product.imageAlt +'">';         
-  domString +=    '</div>';
-  domString +=        '<div class="description">';
-  domString +=          '<p>' + product.description + '</p>';
-  domString +=          '<h6>$' + product.price + '</h6>';
-  domString +=        '</div>';
-  
-  if (product.soldOut) {
-    domString += '<div class="sold-out">';
-    domString +=   '<img src="./images/soldOut.png" alt="Sold Out">';
-    domString +=  '</div>';
-  };
-  
-  domString +=  '</section>';
-  return domString;
-};
-
-function printProductArrayToDom(productArray){
-  for (var i = 0; i < productArray.length; i++) {
-
-  let currentProduct = productArray[i];
-  let productDomString = buildDomString(currentProduct);
-  productContainer.innerHTML += productDomString;
-  };
-};
-
+const addNewProduct = (name, imagePath, imageAlt, description, price, soldOut) => {
+  const newProduct = {name, imagePath, imageAlt, description, price, soldOut};
+  allProducts.push(newProduct);
+}
+addNewProduct("Butt Cream", "http://assets.epicurious.com/photos/57631b4eff66dde1456dfed4/master/pass/sweetened-whipped-cream.jpg", "Here is a picture of butt cream.", "lorem oh well", 4.99, false);
+const productContainer = document.getElementById("product-container");
+    const buildDomString = (product) => {
+    let domString = "";
+    domString +=     '<section class="product">';
+    domString +=       '<div class="title child">';
+    domString +=         '<h2>' + product.name + '</h2>';
+    domString +=       '</div>';
+    domString +=       '<div class="image child">';
+    domString +=         '<img src="'+ product.imagePath +'" alt="' + product.imageAlt + '">';
+    domString +=       '</div>';
+    domString +=       '<div class="description child">';
+    domString +=         '<p>' + product.description +'</p>';
+    domString +=         '<h6>$' + product.price +'</h6>';
+    domString +=       '</div>';
+    if (product.soldOut) {
+      domString += '<div class="sold-out child">';
+      domString +=  '<img src="./images/soldOut.png" alt="Sold Out">';
+      domString += '</div>';
+    }
+    domString +=     '</section>';
+    return domString;
+}
+const printProductArrayToDom = (productArray) => {
+  for (let i = 0; i < productArray.length; i++) {
+    const currentProduct = productArray[i];
+    const productDomString = buildDomString(currentProduct);
+    productContainer.innerHTML += productDomString;
+  }
+}
 printProductArrayToDom(allProducts);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let selectedCard;
+document.getElementById('product-container').addEventListener("click", (event) => {
+  changeTheBorder(event);
+  printSelectedDescription();
+});
+const changeTheBorder = (event) => {
+  if (event.target.classList.contains("child")){
+    selectedCard = event.target.parentNode;
+  } else if (event.target.parentNode.parentNode.classList.contains("product")){
+    selectedCard = event.target.parentNode.parentNode;
+  } else if (event.target.classList.contains("product")){
+    selectedCard = event.target;
+  }
+  selectedCard.classList.add("border-funsies");
+}
+    
+const printSelectedDescription = () => {
+  const description = selectedCard.childNodes[2].childNodes[0].innerHTML;
+  console.log(description);
+}
